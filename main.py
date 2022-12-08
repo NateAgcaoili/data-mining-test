348edce9b37ae5815d50cd38f0f1e49390c35f92
Nate Agcaoili <nateagcaoili.2k@gmail.com>
2022-12-08 00:54:46 -0500

    main fix

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ if classification == '1':
     try:
         t1 = float(input("[FLOAT] Enter mixture ratio threshold: "))
     except ValueError:
-        print("Invalid threshold provided. Defaulting to 10.")
+        print("Invalid threshold provided. Defaulting to 10.\n")
         t1 = 10
     try:
         g = float(input("[FLOAT][0 < g < 0.015] Enter g value: "))
-    except ValueError:
-        print("Invalid g value provided. Defaulting to 0.007")
+        if (g < 0) or (g >=0.015):
+            print("g must be between 0 < g < 0.015. Defaulting to 0.007\n")
         g = 0.007
-    if (g < 0) or (g >=0.015):
-        print("g must be between 0 < g < 0.015. Defaulting to 0.007")
+    except ValueError:
+        print("Invalid g value provided. Defaulting to 0.007\n")
         g = 0.007
     ID3.main(t1, g)
 else:
